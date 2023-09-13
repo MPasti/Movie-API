a3a3218c42332ea1fbead15d92f24266ad410f26
MATHEUS SOARES PASTI <24841@unifacef.edu.br>
2023-09-13 19:43:05 -0300

    adicionando conteudo

---
+++ index.js
@@ -0,0 +1,23 @@
+// esta função é assíncrona
+let buscarCep = async () => {
+  // recupera o valor do campo cep
+  let cep = document.getElementById('cep').value;
+  // monta a url de onde está localizado a API
+  let url = `https://viacep.com.br/ws/${cep}/json/`;
+  // faz a requisição HTTP
+  // await é uma função síncrona, pois enquanto a API não responder,
+  // o código não continua
+  // fecth é uma função nativa do JS que faz requisições HTTP
+  let resposta = await fetch(url);
+  // transforma a resposta em um objeto JSON, pois chegar em string
+  let respostaJSON = await resposta.json(); // a conversão pode demorar
+  // preenche os campos do formulário com os dados do JSON
+  document.getElementById('logradouro').value = respostaJSON.logradouro;
+  document.getElementById('complemento').value = respostaJSON.complemento;
+  if (document.getElementById('complemento').value === '') {
+    document.getElementById('complemento').value = 'Não há complemento';
+  }
+  document.getElementById('bairro').value = respostaJSON.bairro;
+  document.getElementById('uf').value = respostaJSON.uf;
+  document.getElementById('localidade').value = respostaJSON.localidade;
+};
